105a25cb6b1fb72761a2067b624083acc0afa65d
lizl6 <lizl6@asiainfo.com>
2024-05-11 17:52:16 +0800

    删除element-plus引用

--- src/main.js
+++ src/main.js
@@ -2,7 +2,4 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-import ElementPlus from 'element-plus'
-import 'element-plus/theme-chalk/index.css'
-
-createApp(App).use(ElementPlus).mount('#app')
+createApp(App).mount('#app')
